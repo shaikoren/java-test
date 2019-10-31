371003b4dc362eda656f8b382b4dda7e1a1718ee
shaikoren <shai@shaikotech.com>
2019-10-31 21:53:49 +0000

    refactor unit test to parameterised

--- src/test/java/com/henrysgrocery/CashierTest.java
+++ src/test/java/com/henrysgrocery/CashierTest.java
@@ -1,69 +1,54 @@
 package com.henrysgrocery;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+import static com.henrysgrocery.Product.*;
 import static java.time.LocalDate.now;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(Parameterized.class)
 public class CashierTest {
 	private Cashier underTest = new Cashier();
 	private ShoppingBasket shoppingBasket = new ShoppingBasket();
 
-	@Test
-	public void shouldCalculatePriceWithoutDiscount() {
+	private final List<Product> products;
+	private final LocalDate dateOfPurchase;
+	private final double expected;
 
-		shoppingBasket.addProduct(Product.BREAD);
-		shoppingBasket.addProduct(Product.SOUP);
-		shoppingBasket.addProduct(Product.MILK);
-		shoppingBasket.addProduct(Product.APPLE);
-
-		Double result = underTest.calculateTotal(shoppingBasket, now().plusMonths(2));
-
-		assertThat(result).isEqualTo(2.85);
+	public CashierTest(List<Product> products, LocalDate dateOfPurchase, double expected) {
+		this.products = products;
+		this.dateOfPurchase = dateOfPurchase;
+		this.expected = expected;
 	}
 
-	@Test
-	public void shouldCalculatePromotionForApplesWithinPeriod() {
 
-		shoppingBasket.addProduct(Product.APPLE);
-
-		Double result = underTest.calculateTotal(shoppingBasket, now().plusDays(7));
-
-		assertThat(result).isEqualTo(0.09);
+	@Parameters (name = "{index}: when purchasing {0} on {1}, the price should be {2}")
+	public static Collection<Object[]> data() {
+		return asList(new Object[][]{
+				{asList(BREAD, SOUP, MILK, APPLE), now().plusMonths(2), 2.85},
+				{asList(APPLE), now().plusDays(7), 0.09},
+				{asList(APPLE, APPLE), now() , 0.20},
+				{asList(SOUP, SOUP, BREAD), now(), 1.70},
+				{asList(SOUP, SOUP, SOUP, BREAD, BREAD), now(), 3.15}
+		});
 	}
 
-	@Test
-	public void shouldNotApplyPromotionForApplesOutOfPeriod(){
-		shoppingBasket.addProduct(Product.APPLE);
-		shoppingBasket.addProduct(Product.APPLE);
-
-		Double result = underTest.calculateTotal(shoppingBasket, now());
-
-		assertThat(result).isEqualTo(0.20);
-	}
 
 	@Test
-	public void shouldGetHalfPriceBreadWhenBuyTwoSoups(){
-		shoppingBasket.addProduct(Product.SOUP);
-		shoppingBasket.addProduct(Product.SOUP);
-		shoppingBasket.addProduct(Product.BREAD);
+	public void shouldCalculateTotalPrice() {
+		products.forEach(product -> shoppingBasket.addProduct(product));
+		
+		Double result = underTest.calculateTotal(shoppingBasket, dateOfPurchase);
 
-		Double result = underTest.calculateTotal(shoppingBasket, now());
-
-	    assertThat(result).isEqualTo(1.70);
-	}
-
-	@Test
-	public void shouldCalculatePriceWithManyBreads(){
-		shoppingBasket.addProduct(Product.SOUP);
-		shoppingBasket.addProduct(Product.SOUP);
-		shoppingBasket.addProduct(Product.SOUP);
-		shoppingBasket.addProduct(Product.BREAD);
-		shoppingBasket.addProduct(Product.BREAD);
-
-		Double result = underTest.calculateTotal(shoppingBasket, now());
-
-		assertThat(result).isEqualTo(3.15);
+		assertThat(result).isEqualTo(expected);
 	}
 
 }
